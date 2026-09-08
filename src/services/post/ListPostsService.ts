4d2b974f566d05332f6d65218a590f5605892157
david-pessoa <pessoa.dvlbp@gmail.com>
2026-09-08 13:55:09 -0300

    fix: add userId no retorno de GET /posts

--- src/services/post/ListPostsService.ts
+++ src/services/post/ListPostsService.ts
@@ -41,6 +41,7 @@ export class ListPostsService {
         dataCriacao: post.dataCriacao,
         dataModificacao: post.dataModificacao,
         criadoPor: {
+            userId: post.user.id,
             nome: post.user.nome,
             tipoUsuario: post.user.role.nome,
             image: post.user.image ? `/api/user/${post.user.id}/image` : null,
